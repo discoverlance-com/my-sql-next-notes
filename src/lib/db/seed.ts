85b7c9a7a738db5b45aebf31e389903a6ae36a7c
Lance Armah-Abraham <lance@vdc.cloud>
2023-11-02 09:17:33 +0000

    fix: create table

--- src/lib/db/seed.ts
+++ src/lib/db/seed.ts
@@ -1,19 +1,15 @@
 import { sql } from "@vercel/postgres";
 
-async function seedDatabase() {
+export async function seedDatabase() {
   const createTable = await sql`
-        CREATE TABLE IF NOT EXISTS notes {
+        CREATE TABLE IF NOT EXISTS notes (
             id SERIAL PRIMARY KEY,
             title VARCHAR(255) NOT NULL,
             slug VARCHAR(255) UNIQUE NOT NULL,
             description TEXT,
-            "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-            "updatedAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
-            ON UPDATE CURRENT_TIMESTAMP
-        }
+            "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
     `;
-
-  console.log(`Created "notes" table`);
 
   return { createTable };
 }
